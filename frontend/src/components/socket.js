a603818ff649e51b057034f37f0f53f0b2fd1ac9
Navitq <zhenya.nikonov1999@gmail.com>
2024-05-29 14:49:38 +0300

    one more

--- frontend/src/components/socket.js
+++ frontend/src/components/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const URL =
-    process.env.NODE_ENV === "production" ? undefined : "";
+    process.env.NODE_ENV === "production" ? `${process.env.REACT_APP_HOST}` : "";
 
 export const socket = io(URL, {
     autoConnect: false
